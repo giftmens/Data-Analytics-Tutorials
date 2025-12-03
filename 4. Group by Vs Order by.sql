8e8c8c7c00622a5afe35d387f509f96548886a78
giftmens <162301553+giftmens@users.noreply.github.com>
2025-12-03 20:28:58 +0000

    Add files via upload
    tutorials

---
+++ 4. Group by Vs Order by.sql
@@ -0,0 +1,32 @@
+-- GROUP BY
+
+SELECT *
+FROM employee_demographics
+;
+
+SELECT gender
+FROM employee_demographics
+;
+
+SELECT gender, AVG(age), MAX(age), MIN(age), COUNT(age)
+FROM employee_demographics
+GROUP BY gender
+;
+
+SELECT occupation , salary
+FROM employee_salary
+GROUP BY occupation, salary
+;
+
+-- ORDER BY
+
+SELECT *
+FROM employee_demographics
+ORDER BY first_name DESC
+;
+
+SELECT *
+FROM employee_demographics
+ORDER BY gender, age DESC
+;
+
